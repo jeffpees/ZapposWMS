a64bd5b5051ef7d9d69a137968a5fa0d9a5364e0
McCallister <annaleis@amazon.com>
2014-07-16 12:20:12 -0700

    fixed maven comp issues, added jetty plugin

--- src/main/java/WebController.java
+++ src/main/java/WebController.java
@@ -27,8 +27,8 @@ public class WebController {
 
     @RequestMapping(value = "/welcome")
     public String welcome(Model model) {
-        model.addAttribute("greeting", "Welcome to Web Store!");
-        model.addAttribute("tagline", "The one and only amazing webstore");
+        model.addAttribute("greeting", "Welcome!");
+
 
         return "welcome";
     }
